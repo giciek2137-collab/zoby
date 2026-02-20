8c97bc87cd28ee647499ac7ae34e2f437a3339e9
giciek2137-collab <giciek2137@gmail.com>
2026-02-20 13:02:55 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,17 @@
-self.addEventListener("install", e => {
-  self.skipWaiting();
+const CACHE_NAME = "app-cache-v1";
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(["/"]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
